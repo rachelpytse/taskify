e17a4e8e0d115c5e6144a84cd2beeba85a95ffcb
rach <rachelpytse@gmail.com>
2024-03-03 20:58:26 +0100

    Added update card function

---
+++ components/providers/modal-provider.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { CardModal } from "../modals/card-modal"
+
+export const ModalProvider = () => {
+    //prevent hydration error
+    const [isMounted, setIsMounted] = useState(false)
+
+    //ensure everything inside is only rendered on the client bc useEffect is only rendered on the client
+    //so unless it has been mounted, true by the initial mount here, the card modal will not be rendered
+    //and thus it will not be creating any inconsistency when it comes to server and the client
+    //thus preventing hydration error
+    useEffect(() => {
+        setIsMounted(true)
+    }, [])
+
+    if(!isMounted) {
+        return null
+    }
+
+    return(
+        <>
+            <CardModal/>
+        </>
+    )
+}
